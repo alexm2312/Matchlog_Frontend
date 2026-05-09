0be21e54ad8aebe76f3d96975de3fe229a1b07e4
alexm2312 <alexander.meier@student.htw-berlin.de>
2026-05-09 13:05:32 +0200

    Add Matchlog list component

---
+++ src/components/Matchloglist.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const examples = [
+  { id: 1, name: 'Matchlog', type: 'Frontend-Projekt' },
+  { id: 2, name: 'Vue Router', type: 'Routing' },
+  { id: 3, name: 'Komponente', type: 'Wiederverwendbarer Baustein' }
+]
+</script>
+
+<template>
+  <section>
+    <h2>Beispiel-Entitäten</h2>
+
+    <ul>
+      <li v-for="example in examples" :key="example.id">
+        {{ example.name }} – {{ example.type }}
+      </li>
+    </ul>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import Matchloglist from '../components/Matchloglist.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Matchloglist />
   </main>
 </template>
