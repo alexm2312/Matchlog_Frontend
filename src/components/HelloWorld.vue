e9d8c0371b9d7550703d7202a0421b5935e092e6
Leandro <leandro.schadock@vodafone.de>
2026-05-09 13:16:06 +0200

    Paar Texte angepasst und schön gemacht

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,8 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Union Berlin und Kaiserslautern an die Fussballspitze
+      Für immer der FCU und FCK
     </h3>
   </div>
 </template>
